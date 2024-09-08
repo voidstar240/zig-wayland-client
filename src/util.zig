a081b1fcee53bd40f30e649ce1c39540791f6656
Andrew Freuler <andrewfreuler@gmail.com>
2024-09-08 03:46:56 -0400

    refactor: reduce scope of writeRequestRaw function
    Abstract all complex type serialization out of writeRequestRaw function.

--- src/util.zig
+++ src/util.zig
@@ -133,148 +133,102 @@ pub const WaylandState = struct {
     }
 };
 
-/// Gets the length of a request given args.
-fn msgLength(args: anytype) u16 {
-    const ArgsType = @TypeOf(args);
-    const args_info = @typeInfo(ArgsType);
-    if (args_info != .Struct) {
-        @compileError(
-            "expected tuple or struct argument, found " ++ @typeName(ArgsType)
-        );
-    }
-
-    const fields = args_info.Struct.fields;
-    if (fields.len > 20) {
-        @compileError("20 arguments max are supported for wire format");
-    }
-    var len: u16 = 8;
-    inline for (fields) |field| {
-        const val = @field(args, field.name);
-        switch (field.type) {
-            []const u8 => {
-                len += 4;
-                len += @intCast(val.len);
-                len += @intCast(@subWithOverflow(0, val.len).@"0" % 4);
-            },
-            [:0]const u8 => {
-                len += 4;
-                len += @intCast(val.len + 1);
-                len += @intCast(@subWithOverflow(0, val.len + 1).@"0" % 4);
-            },
-            ?[:0]const u8 => {
-                len += 4;
-                if (val == null) continue;
-                len += @intCast(val.len + 1);
-                len += @intCast(@subWithOverflow(0, val.len + 1).@"0" % 4);
-            },
-            else => len += 4,
-        }
-    }
-    return len;
-}
-
 const Header = packed struct(u64) {
     object_id: u32,
     opcode: u16,
     length: u16,
 };
 
+/// Gets the length of a request given args.
+fn calcRequestLength(args: anytype) u16 {
+    const ArgsType = @TypeOf(args);
+    const args_info = @typeInfo(ArgsType);
+
+    var len: u16 = 8;
+    const fields = args_info.Struct.fields;
+    inline for (fields) |field| {
+        const val = @field(args, field.name);
+        switch (field.type) {
+            []const u8 => {
+                len += @intCast(val.len);
+                len += @intCast(@subWithOverflow(0, val.len).@"0" % 4);
+            },
+            [:0]const u8 => {
+                len += @intCast(val.len + 1);
+                len += @intCast(@subWithOverflow(0, val.len + 1).@"0" % 4);
+            },
+            ?[:0]const u8 => {
+                if (val == null) continue;
+                len += @intCast(val.len + 1);
+                len += @intCast(@subWithOverflow(0, val.len + 1).@"0" % 4);
+            },
+            else => {},
+        }
+        len += 4;
+    }
+    return len;
+}
+
+/// Writes a request to `writer` from `object_id` with `opcode` and `args`.
+/// Length is calculated from `args`.
 pub fn writeRequestRaw(
     writer: anytype,
-    obj_id: u32,
+    object_id: u32,
     opcode: u16,
     args: anytype
 ) !void {
     const ArgsType = @TypeOf(args);
     const args_info = @typeInfo(ArgsType);
-    if (args_info != .Struct) {
+    if (args_info != .Struct)
         @compileError("Not tuple or struct, found " ++ @typeName(ArgsType));
-    }
 
+    const header = Header {
+        .object_id = object_id,
+        .opcode = opcode,
+        .length = calcRequestLength(args),
+    };
+    try wireWriteHeader(writer, header);
     const fields = args_info.Struct.fields;
-    if (fields.len > 20) {
-        @compileError("20 arguments max are supported for wire format");
-    }
-
-    try writer.writeAll(&toBytes(obj_id));
-    try writer.writeAll(&toBytes(opcode));
-    try writer.writeAll(&toBytes(msgLength(args)));
     inline for (fields) |field| {
         const info = @typeInfo(field.type);
         const val = @field(args, field.name);
+        const field_type_name = @typeName(field.type);
         switch (field.type) {
-            i32, u32 => try writer.writeAll(&toBytes(val)),
-            Fixed => try writer.writeAll(&toBytes(@as(i32, @intFromEnum(val)))),
-            []const u8 => {
-                try writer.writeAll(&toBytes(@as(u32, val.len)));
-                try writer.writeAll(val);
-                const pad = @subWithOverflow(0, val.len).@"0" % 4;
-                for (0..pad) |_| {
-                    try writer.writeAll(&[_]u8{0});
-                }
-            },
-            [:0]const u8 => {
-                try writer.writeAll(&toBytes(@as(u32, val.len + 1)));
-                try writer.writeAll(val);
-                try writer.writeAll(&[_]u8{0});
-                const pad = @subWithOverflow(0, val.len + 1).@"0" % 4;
-                for (0..pad) |_| {
-                    try writer.writeAll(&[_]u8{0});
-                }
-            },
-            ?[:0]const u8 => {
-                if (val == null) {
-                    try writer.writeAll(&toBytes(@as(u32, 0)));
-                    continue;
-                }
-                try writer.writeAll(&toBytes(@as(u32, val.len + 1)));
-                try writer.writeAll(val);
-                try writer.writeAll(&[_]u8{0});
-                const pad = @subWithOverflow(0, val.len + 1).@"0" % 4;
-                for (0..pad) |_| {
-                    try writer.writeAll(&[_]u8{0});
-                }
-            },
-            *const Object, Object => {
-                try writer.writeAll(&toBytes(val.id));
-            },
-            ?*const Object, ?Object => {
-                try writer.writeAll(&toBytes(if (val) |o| o.id else 0));
-            },
+            i32 => try wireWriteI32(writer, val),
+            u32 => try wireWriteU32(writer, val),
+            Fixed => try wireWriteFixed(writer, val),
+            []const u8 => try wireWriteArray(writer, val),
+            [:0]const u8 => try wireWriteString(writer, val),
+            ?[:0]const u8 => try wireWriteString(writer, val),
+            Object => try wireWriteObject(writer, val),
+            ?Object => try wireWriteObjectOptional(writer, val),
             else => switch (info) {
-                .Pointer, .Struct => {
-                    if (@hasField(field.type, "inner")) {
+                .Struct => {
+                    if (!@hasField(field.type, "inner")) {
                         @compileError("No inner field on struct.");
                     }
                     const obj = val.inner;
                     if (@TypeOf(obj) != Object) {
                         @compileError("inner is not an Object.");
                     }
-                    try writer.writeAll(&toBytes(obj.id));
+                    try wireWriteObject(writer, obj);
                 },
                 .Optional => {
                     if (val) |interface| {
-                        if (@hasField(@TypeOf(interface), "inner")) {
-                            @compileError("No inner field on struct");
+                        if (!@hasField(@TypeOf(interface), "inner")) {
+                            @compileError("No inner field on struct.");
                         }
-                        const obj = val.inner;
+                        const obj = interface.inner;
                         if (@TypeOf(obj) != Object) {
                             @compileError("inner is not an Object.");
                         }
-                        try writer.writeAll(&toBytes(obj.id));
+                        try wireWriteObject(writer, obj);
                     } else {
-                        try writer.writeAll(&toBytes(@as(u32, 0)));
+                        try wireWriteU32(writer, 0);
                     }
                 },
-                .Enum => |meta| {
-                    if (meta.tag_type != i32 or meta.tag_type != u32) {
-                        @compileError("Enum tag isn't u32/i32.");
-                    }
-                    try writer.writeAll(&toBytes(@intFromEnum(val)));
-                },
-                else => @compileError(
-                    "Unrecognized type, " ++ @typeName(field.type)
-                ),
+                .Enum => try wireWriteEnum(writer, val),
+                else => @compileError("Invalid type, " ++ field_type_name),
             },
         }
     }
@@ -360,4 +314,79 @@ pub fn decodeEvent(event: []const u8, Type: type) !Type {
     }
 
     return out;
+}
+
+fn wireWriteHeader(writer: anytype, header: Header) !void {
+    return writer.writeAll(&toBytes(header));
+}
+
+fn wireWriteI32(writer: anytype, num: i32) !void {
+    return writer.writeAll(&toBytes(num));
+}
+
+fn wireWriteU32(writer: anytype, num: u32) !void {
+    return writer.writeAll(&toBytes(num));
+}
+
+fn wireWriteFixed(writer: anytype, num: Fixed) !void {
+    return wireWriteI32(writer, @intFromEnum(num));
+}
+
+fn wireWriteFD(writer: anytype, fd: FD) !void {
+    // TODO send fd through ancillary data
+    return wireWriteI32(writer, fd);
+}
+
+fn wireWriteArray(writer: anytype, arr: []const u8) !void {
+    try wireWriteU32(writer, @intCast(arr.len));
+    try writer.writeAll(arr);
+    const pad = @subWithOverflow(0, arr.len).@"0" % 4;
+    for (0..pad) |_| {
+        try writer.writeAll(&[_]u8{0});
+    }
+}
+
+fn wireWriteString(writer: anytype, str: ?[:0]const u8) !void {
+    if (str == null) {
+        return wireWriteU32(writer, 0);
+    }
+    const len = str.?.len + 1;
+    try wireWriteU32(writer, @intCast(len));
+    try writer.writeAll(str.?[0..len]);
+    const pad = @subWithOverflow(0, len).@"0" % 4;
+    for (0..pad) |_| {
+        try writer.writeAll(&[_]u8{0});
+    }
+}
+
+fn wireWriteObject(writer: anytype, object: anytype) !void {
+    const type_name = @typeName(@TypeOf(object));
+    const type_info = @typeInfo(@TypeOf(object));
+    if (type_info != .Struct)
+        @compileError(type_name ++ " is not an object. Not a struct.");
+    if (!@hasField(object, "id"))
+        @compileError(type_name ++ " is not an object. Has no `id` field.");
+    if (@TypeOf(object.id) != u32)
+        @compileError(type_name ++ " is not an object. `id` not u32.");
+
+    return wireWriteU32(writer, object.id);
+}
+
+fn wireWriteObjectOptional(writer: anytype, object: anytype) !void {
+    if (object == null)
+        return wireWriteU32(writer, 0);
+    return wireWriteObject(writer, object.?);
+}
+
+fn wireWriteEnum(writer: anytype, enum_: anytype) !void {
+    const type_name = @typeName(@TypeOf(enum_));
+    const type_info = @typeInfo(@TypeOf(enum_));
+    if (type_info != .Enum)
+        @compileError(type_name ++ " is not an object. Not a struct.");
+    
+    switch (type_info.Enum.tag_type) {
+        i32 => return wireWriteI32(writer, @intFromEnum(enum_)),
+        u32 => return wireWriteU32(writer, @intFromEnum(enum_)),
+        else => @compileError(type_name ++ " tag must be i32 or u32.")
+    }
 }
