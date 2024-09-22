d0fab8eee8dc1c110653b342038592a65ec65fd9
Andrew Freuler <andrewfreuler@gmail.com>
2024-09-21 21:50:57 -0400

    test: replace and rename functionality test
    Remove functionality test and replace with RequestResponse test which
    calls the sync request on wl_display.

--- src/tests.zig
+++ src/tests.zig
@@ -1,34 +1,15 @@
 const std = @import("std");
 const zwl = @import("zwayland");
 
-test "functionality" {
-    const sock = try zwl.connectToSocket();
-    defer sock.close();
-    var global = zwl.WaylandState.init(sock);
+test "SyncRequestResponse" {
+    const socket = try zwl.connectToSocket();
+    defer socket.close();
+    var state = zwl.WaylandState.init(socket);
     const display = zwl.WaylandState.getDisplay();
-    const reg = try display.getRegistry(&global, 2);
-    const sync = try display.sync(&global, 3);
+    const sync = try display.sync(&state, 2);
 
-    while (true) {
-        const anon = try global.readEvent();
-        if (try reg.decodeGlobalEvent(anon)) |event| {
-            std.debug.print(
-                \\{d}: {s} v{d}
-                \\
-                , .{event.name, event.interface, event.version});
-        } else if (try sync.decodeDoneEvent(anon)) |_| {
-            std.debug.print("DONE!\n", .{});
-            break;
-        } else if (try display.decodeErrorEvent(anon)) |event| {
-            std.debug.print(
-                \\ERROR: {d} on object {d}: {s}
-                \\
-                , .{event.code, event.object_id.id, event.message});
-        } else {
-            std.debug.print(
-                \\Unkown Event: obj: {d} op: {d}\n
-                \\
-                , .{anon.self.id, anon.opcode});
-        }
+    const anon_event = try state.readEvent();
+    if (try sync.decodeDoneEvent(anon_event) == null) {
+        return error.NotSyncEvent;
     }
 }
